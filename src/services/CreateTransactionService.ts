d76dc9c25df25618ae5da2cff54e0b41544f7244
Lubota Ngola Joao <lubota4@gmnail.com>
2020-06-18 19:45:38 -0300

    Desafio Fundamentos NodeJS

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,38 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+	title: string;
+	value: number;
+	type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+	private transactionsRepository: TransactionsRepository;
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
+	constructor(transactionsRepository: TransactionsRepository) {
+		this.transactionsRepository = transactionsRepository;
+	}
 
-  public execute(): Transaction {
-    // TODO
-  }
+	public execute({ title, value, type }: Request): Transaction {
+		if (!['income', 'outcome'].includes(type)) {
+			throw new Error('Invalide transaction type');
+		}
+
+		const { total } = this.transactionsRepository.getBalance();
+
+		if (type === 'outcome' && total < value) {
+			throw new Error('You do not have enough balance');
+		}
+
+		const transation = this.transactionsRepository.create({
+			title,
+			value,
+			type,
+		});
+
+		return transation;
+	}
 }
 
 export default CreateTransactionService;
